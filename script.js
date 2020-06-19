5cd354c2391174c9ce9b0643686a99525e1fed8e
ShanderB <s.hander8910@hotmail.com>
2020-06-19 16:59:13 -0300

    Throw all chat in a txt file (without formatation)

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 
+// var textBox = document.getElementsByClassName(
+//     '_3Whw5 selectable-text invisible-space copyable-text')[0]
+// .textContent;
 
 
+
+
+
+    //Pegar o nome do chat aberto e jogar no nome do arquivo.(How to);
+    //
+
+var textBoxUpdated=[];
+var textBox = document.getElementsByClassName('_3Whw5 selectable-text invisible-space copyable-text');
+// get all elements
 
 
 function download(data, filename, type) {
-    var textbox = document.getElementsByClassName('_3Whw5 selectable-text invisible-space copyable-text').textContent;
-    var file = new Blob([data ], {type: type});
-    var a = document.createElement("a"),
+ 
+    for(let i = 0; i < textBox.length; i++){
+        textBoxUpdated[i] = textBox[i].textContent + "\n"; //Get all text element and textBoxUpdated will receive.
+    }
+
+    var file = new Blob([data], {type: type});
+    var a = document.createElement("a")
     url = URL.createObjectURL(file);
         a.href = url;
         a.download = filename;
-        document.body.appendChild(a);
+        document.body.appendChild(a);                     
         a.click();
         setTimeout(function() {
             document.body.removeChild(a);
@@ -18,8 +34,9 @@ function download(data, filename, type) {
         }, 0); 
     }
 
+    // Function from
+    // https://stackoverflow.com/questions/13405129/javascript-create-and-save-file
 
 
 
-
-download('file text', 'myfilename.txt', 'text/plain')
+download(textBoxUpdated, "myFile.txt", 'text/plain')
